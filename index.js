0d1f5452eae93f94d3fb364612d52413666cbe6c
Michael Pope <popemray1986@gmail.com>
2018-06-30 16:59:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function showRepositories(event, date) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
-
   document.getElementById("repositories").innerHTML = repoList
 }
 
